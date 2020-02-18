121744ee26e20313b410b37ad3883db0d53366ab
varad2011 <patilvarad2011@gmail.com>
2020-02-18 17:37:12 +0530

    added archieve and archieve method with pin unpin logic

--- fundoo/src/main/java/com/bridgelabz/FundooNote/service/NoteService.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/service/NoteService.java
@@ -1,18 +1,18 @@
 package com.bridgelabz.FundooNote.service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.FundooNote.Util.TokenGeneratorDecoder;
 import com.bridgelabz.FundooNote.dto.NoteDto;
 import com.bridgelabz.FundooNote.model.NoteModel;
-import com.bridgelabz.FundooNote.model.RecycleBinModel;
 import com.bridgelabz.FundooNote.model.RegistrationModel;
 import com.bridgelabz.FundooNote.repository.NoteRepository;
-import com.bridgelabz.FundooNote.repository.RecycleBinRepository;
 import com.bridgelabz.FundooNote.repository.RegistrationPageRepository;
 import com.bridgelabz.FundooNote.response.Response;
 
@@ -34,7 +34,9 @@ public class NoteService {
 	 * @Autowired private RecycleBinModel recycleBinModel;
 	 */
 	  
-	  public boolean checkUserAvailabilty(String token) {
+	
+	//user availability check
+	  public boolean checkUserAvailability(String token) {
 		  System.out.println(token);
 			String userId = tokenDecoder.decodeToken(token);
 			System.out.println(userId);
@@ -48,6 +50,8 @@ public class NoteService {
 			}
 	  }
 	
+	  
+	  //new note create
 	public Response createNewNote(NoteModel model, String token) {
 		
 		long id = Long.parseLong(tokenDecoder.decodeToken(token));
@@ -69,6 +73,7 @@ public class NoteService {
 		}
 	}
 	
+	//deleteNote
 	public Response deleteNote(NoteDto model, String token) {
 		
 		long id = Long.parseLong(tokenDecoder.decodeToken(token));
@@ -91,6 +96,7 @@ public class NoteService {
 		}
 	}
 
+	//update create notes
 	public Response updateNote(NoteDto model, String token) {
 		long id = Long.parseLong(tokenDecoder.decodeToken(token));
 		long noteId = model.getNoteId();
@@ -115,18 +121,134 @@ public class NoteService {
 		}
 	}
 
-	public List<NoteModel> Displaydata(String token) {
+	// display all user create Notes
+	public List<NoteModel> displayNote(String token) {
 		long id = Long.parseLong(tokenDecoder.decodeToken(token));
 		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
 		
 		if(registrationModel.isPresent()) {
 			List<NoteModel> noteModel = noteRepsitory.findAll();
-			return noteModel ;
+			List<NoteModel> noteModel1 = noteModel.stream().filter(t -> (t.getModel().getId())==(id)).collect(Collectors.toList());
+			//System.out.println(noteModel);
+			return noteModel1;
 		}else {
 			return null;
 		}
-		
 	}
-	
-	
+
+	//Sort note by list
+	public List<NoteModel> displayUserNotes(String name, String token) {
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		
+		if(registrationModel.isPresent()) {
+			List<NoteModel> noteModel = noteRepsitory.findAll();
+			System.out.println(noteModel);
+			List<NoteModel> noteModel1 = noteModel.stream().filter(t -> t.getTitle().equals(name)).sorted().collect(Collectors.toList());
+			return noteModel1;
+		}else {
+			return  null;
+		}
+	}
+
+	//store note to trash
+	public Response storeToTrash(long noteId, String token) {
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		Optional<NoteModel> noteModel = noteRepsitory.findByNoteId((int) noteId);
+		
+		if(registrationModel.isPresent()) {
+			
+			if(noteModel.get().isPinUnpin()) {
+				
+				noteModel.get().setPinUnpin(false);
+			}
+			noteModel.get().setTrash(true);
+			noteModel.get().setAtModified();
+			noteRepsitory.save(noteModel.get());
+			return new Response(200, "trash successfully", null);
+		}else{
+			return new Response(400, "not trush", null);
+		}
+	}
+
+	//delete notes from trash permanent deleted data 
+	public Response deleteFromTrash(int noteId, String token) {
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		Optional<NoteModel> noteModel = noteRepsitory.findByNoteId( noteId);
+		
+		if((registrationModel.isPresent()) && (noteModel.get().isTrash())) {
+			noteRepsitory.deleteById(noteId);
+		
+			return new Response(200, "delete successfully from trash", null);
+		}else {
+			return new Response(400, "not delete from trush", null);
+		}
+	}
+
+	//pin unpin  notes
+	//if note is pin then unpin it or if note is unpin then pin it 
+	public Response pinUnpin(int noteId, String token) {
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		Optional<NoteModel> noteModel = noteRepsitory.findByNoteId( noteId);
+		if(registrationModel.isPresent()) {
+			
+			if(!noteModel.get().isPinUnpin()) {
+				noteModel.get().setPinUnpin(true);
+			}else {
+				noteModel.get().setPinUnpin(false);
+			}
+			noteRepsitory.save(noteModel.get());
+			
+			return new Response(200, "note pin Status change", null);
+		}else {
+			return new Response(400, " not status change", null);
+		}
+	}
+
+	//note added to archieve 
+	public Response addToArchieve(int noteId, String token) {
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		Optional<NoteModel> noteModel = noteRepsitory.findByNoteId((int) noteId);
+		
+		if((registrationModel.isPresent())&& (!noteModel.get().isTrash())) {
+			
+			if(noteModel.get().isPinUnpin()) {
+				noteModel.get().setPinUnpin(false);
+				noteModel.get().setArchievePin(true);
+			}
+			noteModel.get().setArchieve(true);
+			noteModel.get().setAtModified();
+			noteRepsitory.save(noteModel.get());
+			
+			return new Response(200, "note added to archieve", null);	
+		}else {
+			return new Response(400, "not added to archieve", null);
+		}
+	}
+
+	//note unArchieve 
+	public Response unArchieveNote(int noteId, String token) {
+		
+		long id = Long.parseLong(tokenDecoder.decodeToken(token));
+		Optional<RegistrationModel> registrationModel = registrationPagerepository.findById(id);
+		Optional<NoteModel> noteModel = noteRepsitory.findByNoteId((int) noteId);
+		
+		if(registrationModel.isPresent())  {
+			
+			if(noteModel.get().isArchievePin()) {
+				noteModel.get().setPinUnpin(true);
+			}
+			noteModel.get().setArchievePin(false);
+			noteModel.get().setArchieve(false);
+			noteRepsitory.save(noteModel.get());
+			
+			return new Response(200, " unArchieve successfully", null);
+		}else {
+			return new Response(200, "not  unArchieve", null);
+		}
+	}
 }
