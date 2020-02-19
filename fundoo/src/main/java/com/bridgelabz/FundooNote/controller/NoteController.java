ce98615d913c0b7a424323845b274cea691ec65d
varad2011 <patilvarad2011@gmail.com>
2020-02-19 18:59:19 +0530

    added untrash uri

--- fundoo/src/main/java/com/bridgelabz/FundooNote/controller/NoteController.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/controller/NoteController.java
@@ -19,49 +19,70 @@ import com.bridgelabz.FundooNote.service.NoteService;
 public class NoteController {
 	
 	@Autowired
-	private NoteService service;
+	private NoteService noteService;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/noteCreate")
 	public Response createNote(@RequestBody NoteModel model, @RequestParam String token) {
-		
-		Response response = service.createNewNote(model, token);
-		
+		Response response = noteService.createNewNote(model, token);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/deleteNote")
 	public Response deleteNote(@RequestBody NoteDto model, @RequestParam String token) {
-		Response response = service.deleteNote(model, token);
+		Response response = noteService.deleteNote(model, token);
 		return response;
 	}
 	
 	@RequestMapping(method =  RequestMethod.POST, value = "/updateNote")
 	public Response update(@RequestBody NoteDto model, @RequestParam String token) {
-		Response response = service.updateNote(model, token);
+		Response response = noteService.updateNote(model, token);
 		return response;
 	}
 	
 	@RequestMapping(method =  RequestMethod.GET, value = "/noteName")
 	public List<NoteModel> sortList( @RequestParam String noteName, @RequestParam String token) {
-		List<NoteModel> noteModel = service.displayUserNotes(noteName, token);
+		List<NoteModel> noteModel = noteService.displayUserNotes(noteName, token);
 		return noteModel ;
 	}
 	
 	@RequestMapping(method =  RequestMethod.POST, value = "/trash")
 	public Response trash(@RequestParam Long noteId, @RequestParam String token) {
-		Response response = service.storeToTrash(noteId, token);
+		Response response = noteService.storeToTrash(noteId, token);
 		return response;
 	}
 	
+	@RequestMapping(method =  RequestMethod.POST, value = "/trashToList")
+	public Response trashToList(@RequestParam Long noteId, @RequestParam String token) {
+		Response response = noteService.trashToList(noteId, token);
+		return response;
+	}
 	@RequestMapping(method =  RequestMethod.POST, value = "/deleteFromTrash")
 	public Response deleteFromTrash(@RequestParam int noteId, @RequestParam String token) {
-		Response response = service.deleteFromTrash(noteId, token);
+		Response response = noteService.deleteFromTrash(noteId, token);
 		return response;
 	}
 	
 	@RequestMapping(method =  RequestMethod.POST, value = "/pinUnpin")
 	public Response pinUnpin(@RequestParam int noteId, @RequestParam String token) {
-		Response response = service.pinUnpin(noteId, token);
+		Response response = noteService.pinUnpin(noteId, token);
+		return response;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/addToArchieve")
+	public Response addToArchieve (@RequestParam int noteId, @RequestParam String token) {
+		Response response = noteService.addToArchieve(noteId, token);
+		return response;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/unArchieve")
+	public Response unArchieve(@RequestParam int noteId, @RequestParam String token) {
+		Response response = noteService.unArchieveNote(noteId, token);
+		return response;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/collaborate")
+	public Response collaborator(@RequestBody NoteModel model, String emailId, String token) {
+		Response response = noteService.collaborateWithEmailId(model, emailId, token);
 		return response;
 	}
 }
