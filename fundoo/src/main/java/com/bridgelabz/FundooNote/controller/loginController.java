7475f23a9df1d297d48e4786ff1e4aa21f94f1b7
varad2011 <patilvarad2011@gmail.com>
2020-02-26 13:47:17 +0530

    remove : Code which are not in use

--- fundoo/src/main/java/com/bridgelabz/FundooNote/controller/loginController.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/controller/loginController.java
@@ -1,13 +1,6 @@
 package com.bridgelabz.FundooNote.controller;
 
-import java.io.UnsupportedEncodingException;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,29 +14,11 @@ import com.bridgelabz.FundooNote.service.LoginService;
 public class loginController {
 
 	@Autowired
-	private LoginService service;
+	private LoginService loginService;
 	
-	@RequestMapping(method = RequestMethod.POST, value = "login")
-	public String loginCheck(@RequestBody UserLoginDto loginDto) throws UnsupportedEncodingException {
-		System.out.println(loginDto);
-		if(service.userAvailabilityCheck(loginDto) == true) {
-			return "login success";
-		}else {
-			return "invalid user";
-		}
-	}
-
-	/*
-	 * @RequestMapping(method = RequestMethod.POST, value = "login1") public
-	 * ResponseEntity<Response> login(@RequestBody UserLoginDto loginDto ) throws
-	 * UnsupportedEncodingException{ Response response =
-	 * service.userAvailabilityCheck1(loginDto);
-	 * 
-	 * return new ResponseEntity<Response>(response, HttpStatus.OK); }
-	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/userLogin")
 	public Response loginUser(@RequestBody UserLoginDto loginDto) {
-			Response response = service.userLoginCheck(loginDto);
+			Response response = loginService.userLoginCheck(loginDto);
 		return response;
 	}
 }
