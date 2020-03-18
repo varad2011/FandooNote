b83922ae6b5172443a4d3fb830f312bdf49c34c8
varad2011 <patilvarad2011@gmail.com>
2020-03-18 18:57:14 +0530

    replace exception name by message

--- fundoo/src/main/java/com/bridgelabz/FundooNote/response/RecordNotFoundException.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/response/RecordNotFoundException.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RecordNotFoundException extends RuntimeException {
 	
-	public RecordNotFoundException(String exception) {
-	super(exception);
+	public RecordNotFoundException(String message) {
+	super(message);
 	}
 	
 
