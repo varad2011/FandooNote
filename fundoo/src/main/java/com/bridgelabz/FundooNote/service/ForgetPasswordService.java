8106ca6fbb38c0b979ca99875a016c06e38a7967
varad2011 <patilvarad2011@gmail.com>
2020-03-18 18:56:34 +0530

    remove methods and put in registration

--- fundoo/src/main/java/com/bridgelabz/FundooNote/service/ForgetPasswordService.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/service/ForgetPasswordService.java
@@ -41,26 +41,4 @@ public class ForgetPasswordService {
 
 	
 
-	public Response checkUserEmailId(ForgetPasswordDto passwordDto) {
-		System.out.println(passwordDto.getEmailId());
-		String emailId = passwordDto.getEmailId();
-		Optional<RegistrationModel> model = registrationPageRepository.findByEmailId(emailId);
-		System.out.println(model);
-			if(!model.isPresent()) {
-				return new Response(200, "wrong emailId Enter",null);
-			}else {
-				email.sendEmail(null, "click on link to forget pasword", emailId, "http://localhost:3000/login/resetPassword");
-				return new Response(200, "send link on mail to forget password",null);
-			}
-	}
-
-	public Response updatePassword(UserLoginDto resetPasswordDto) {
-		String emailId = resetPasswordDto.getEmailId();
-		String password = resetPasswordDto.getPassword();
-		RegistrationModel model = RegistrationRepository.findByEmailId(emailId);
-		model.setPassword(passwordEncoder.encode(password));
-		RegistrationRepository.save(model);
-		System.out.println(model);
-		return new Response(200, "password reset successfully", null);
-	}
 }
