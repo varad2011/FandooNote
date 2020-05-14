c6c10d31854c404e80364a7746ff2467bb11e5e8
varad <varad2011>
2020-05-14 22:38:13 +0530

    added deploy vertical method

--- fundoo/src/main/java/com/bridgelabz/FundooNote/FundooApplication.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/FundooApplication.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.FundooNote;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import com.bridgelabz.FundooNote.vertX.ServerVertical;
+
+import io.vertx.core.Vertx;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableScheduling
+//@EnableScheduling
 public class FundooApplication {
-
+//	@Autowired
+//	ServerVertical simpleVertx;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FundooApplication.class, args);
 	}
 
+//	@PostConstruct
+//	public void deployVerticle() {
+//	Vertx.vertx().deployVerticle(simpleVertx);
+//	}
 }
