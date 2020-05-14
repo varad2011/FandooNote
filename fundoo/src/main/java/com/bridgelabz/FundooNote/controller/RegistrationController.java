51e3fd48d8711de2dd09d87d5ba945ae70f7bafd
varad <varad2011>
2020-05-14 22:51:18 +0530

    added user profile set controller method

--- fundoo/src/main/java/com/bridgelabz/FundooNote/controller/RegistrationController.java
+++ fundoo/src/main/java/com/bridgelabz/FundooNote/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.FundooNote.dto.ForgetPasswordDto;
@@ -24,31 +25,39 @@ import javassist.NotFoundException;
 @CrossOrigin
 @RequestMapping("/registration")
 public class RegistrationController {
-	
+
 	@Autowired
 	private RegistrationService registrationService;
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/userRegistration")
 	public ResponseEntity<Response> registration(@Valid @RequestBody RegistrationModel model) {
 		Response response = registrationService.newRegisterEntry(model);
-		return new ResponseEntity<Response>(response, HttpStatus.FOUND);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/userLogin")
-	public ResponseEntity<Response>  loginUser(@RequestBody UserLoginDto loginDto) throws Exception {
-			Response response = registrationService.userLoginCheck(loginDto);
-		return new ResponseEntity<Response>(response, HttpStatus.FOUND);
+	public ResponseEntity<Response> loginUser(@RequestBody UserLoginDto loginDto) throws Exception {
+		Response response = registrationService.userLoginCheck(loginDto);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/resetLink")
-	public  ResponseEntity<Response>  linksendToResetPassword(@RequestBody ForgetPasswordDto passwordDto) throws NotFoundException {
+	public ResponseEntity<Response> linksendToResetPassword(@RequestBody ForgetPasswordDto passwordDto)
+			throws NotFoundException {
 		Response response = registrationService.checkUserEmailId(passwordDto);
-		return new ResponseEntity<Response>(response, HttpStatus.FOUND);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
+
 	@RequestMapping(method = RequestMethod.POST, value = "/resetemailId")
 	public Response forgetUserPassword(@RequestBody UserLoginDto resetPasswordDto) {
 		System.out.println(resetPasswordDto.toString());
 		Response response = registrationService.updatePassword(resetPasswordDto);
+		return response;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/updateUserProfilePic")
+	public Response userProfilePicSet(@RequestParam String token) {
+		Response response = registrationService.updateProfilePic(token);
 		return response;
 	}
 }
